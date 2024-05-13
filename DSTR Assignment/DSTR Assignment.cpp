78bfe2709357e5462b4aa72d30d5db414b214dae
TawfikAlhaja <tawfikalhaja@gmail.com>
2024-05-13 19:19:27 +0800

    I added my code
    The code provided is to shuffle and push the questions to the linked list, Then there is the method to display the questions and answers

--- DSTR Assignment/DSTR Assignment.cpp
+++ DSTR Assignment/DSTR Assignment.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
-using namespace std;
+#include "TriviaHeader.hpp"
+#include <ctime> // Include for time()
 
-int main()
-{
-    cout << "Hello World!\n";
+int main() {
+    // Create an instance of TriviaStack
+    TriviaStack ts;
+
+    // Optionally seed the random number generator to get different shuffling results
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    // Load questions from the file, which will now randomly insert (shuffle) them
+    ts.loadQuestions("C:\\Users\\twfqt\\Downloads\\Questions.txt");
+
+    // Displaying questions to check if they're loaded and shuffled correctly
+    cout << "Shuffled Trivia Questions:" << endl;
+    while (!ts.isempty()) {
+        Node* question = ts.pop(); // pop() also removes the question from the stack
+        cout << "Question: " << question->question << endl;
+        cout << "Answer: " << question->answer << endl;
+        cout << "Points: " << question->points << endl << endl;
+        delete question; // Free memory after displaying
+    }
+
+    return 0;
 }
